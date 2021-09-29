fdbc3fc012b56acc6506261bfdfdec0d7efda88d
Ayen <201811082@fit.edu.ph>
2021-09-29 10:28:40 +0800

    Added multiply and division.Created new file.

--- calculator.cpp
+++ calculator.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main()
 {
-    int firstNumber, secondNumber, sumOfTwoNumbers, difOfTwoNumbers ;
+    int firstNumber, secondNumber, sumOfTwoNumbers, difOfTwoNumbers, prodOfTwoNumbers, quotientOfTwoNumbers ;
     
     cout << "Enter first number: ";
     cin >> firstNumber;
@@ -14,10 +14,15 @@ int main()
     // sum of two numbers is stored in variable sumOfTwoNumbers
     sumOfTwoNumbers = firstNumber + secondNumber;
     difOfTwoNumbers = firstNumber - secondNumber;
+    prodOfTwoNumbers = firstNumber * secondNumber;
+    quotientOfTwoNumbers =  firstNumber / secondNumber;
 
     // Prints sum 
     cout << endl << firstNumber << " + " <<  secondNumber << " = " << sumOfTwoNumbers;     
-    cout << endl << firstNumber << " - " <<  secondNumber << " = " << difOfTwoNumbers;     
+    cout << endl << firstNumber << " - " <<  secondNumber << " = " << difOfTwoNumbers;   
+    cout << endl << firstNumber << " * " <<  secondNumber << " = " << prodOfTwoNumbers;   
+    cout << endl << firstNumber << " / " <<  secondNumber << " = " << quotientOfTwoNumbers;       
+    
 
 
     return 0;
